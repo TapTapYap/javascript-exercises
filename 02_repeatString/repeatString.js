5e96ed43a3c81bb4cb4935879f80fdadd129823f
Patrick Oser <patrick.oser148@gmail.com>
2022-12-15 18:31:23 -0500

    Missing a semicolon somehwere on the firt line and need to rework the for loop.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
 const repeatString = function() {
+    
+    let hey = prompt("What is for horses but better for cows?");
+    
+    for (i = 0; i <= 3; i++) {
+        if (hey == "hey") {
+            return ('hey');
+        } else {
 
+        }
+    }
 };
 
 // Do not edit below this line
