761e6b9f98e3d004b599a969f1828ee0ae72d16e
Patrick Oser <patrick.oser148@gmail.com>
2022-12-18 11:47:11 -0500

    Didn't hit save.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers, num) {
+    const numbers = [];
 
+    for (const num of numbers)
 };
 
 // Do not edit below this line
