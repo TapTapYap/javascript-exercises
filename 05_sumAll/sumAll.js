105efe7814aab30f716894f583689e742c24add2
Patrick Oser <patrick.oser148@gmail.com>
2022-12-23 11:49:40 -0500

    Solution found to 'summAll'.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(isNaN(a) || isNaN(b) || typeof a == "string" || typeof b == "string"){
+        return 'ERROR';
+    }
+    if(a < 0 || b < 0) { 
+        return 'ERROR';
+    }
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let sum = 0;
+    for (let j = min; j <= max; j++) {
+        sum = sum + j;
+    }
+    return sum;
 };
 
 // Do not edit below this line
