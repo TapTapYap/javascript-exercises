7bf2ebd65a66459e03dd0dd2e6a1c49f4051032e
Patrick Oser <patrick.oser148@gmail.com>
2022-12-23 09:46:18 -0500

    All tests pass, assignment complete.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,14 @@
-const removeFromArray = function(itemArray, itemsToRemove) {
-    return itemArray.filter(items => !itemsToRemove.prototype.includes(itemsToRemove));
-};
-
-
+const removeFromArray = function() {
+    let args = Array.prototype.slice.call(arguments);
+    let arr = args[0];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 1; j < args.length; j++) {
+        if (arr[i] === args[j]) {
+          delete arr[i];
+        }
+      }
+    }
+    return arr.filter(Boolean);
+  }
 // Do not edit below this line
 module.exports = removeFromArray;
