127316b7ddead01fb64155f283756d397d0e3441
Patrick Oser <patrick.oser148@gmail.com>
2022-12-24 11:44:40 -0500

    Satified all of the tests for 'tempCoversion'.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFah) {
+    let resultCel = (tempFah - 32) * (5/9);
+    let resultCelRounded = (Math.round(resultCel * 10) / 10);
+    return resultCelRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+    let resultFah = (tempCel * (9 / 5)) + 32;
+    let resultFahRounded = (Math.round(resultFah * 10) / 10);
+    return resultFahRounded;
 };
 
 // Do not edit below this line
