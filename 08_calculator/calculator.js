44bb51d79cd30fc194e34f86411565786942d3a8
Patrick Oser <patrick.oser148@gmail.com>
2023-02-04 10:19:33 -0500

    Done with calculator.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let result = (a + b);
+	return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let result = (a - b);
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(arraySum) {
+	let result = 0;
+  for (i = 0; i < arraySum.length; i++) {
+    result += arraySum[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(mArray) {
+  let result = 1;
+  for (i = 0; i < mArray.length; i++) {
+    result *= mArray[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = Math.pow(a, b);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+
+  for (let i = 2; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
