63a422328cdc45dfb17d57a84f18107b2969ccb1
Patrick Oser <patrick.oser148@gmail.com>
2023-02-05 10:23:53 -0500

    All tests satified for fib ex.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 0;
+    let b = 1;
 
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
+    
+    for (let i = 1; i < num; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
